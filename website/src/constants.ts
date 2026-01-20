37d13b90a7808c4b6ae2466c8e23458da2b794d3
Ignasi Barrera <ignasi@tetrate.io>
2026-01-20 23:19:02 +0100

    minor adjustments for style
    Signed-off-by: Ignasi Barrera <ignasi@tetrate.io>

--- website/src/constants.ts
+++ website/src/constants.ts
@@ -2,4 +2,5 @@
 
 export const GITHUB_REPO = 'https://github.com/tetratelabs/envoy-ecosystem';
 export const GITHUB_REPO_RAW = 'tetratelabs/envoy-ecosystem';
+export const SLACK = 'https://tetr8.io/tetrate-community';
 export const EXTENSIONS_PATH = 'extensions';
